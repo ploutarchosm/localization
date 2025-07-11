d227566f2109a50c20daa8c2f118b15f83bab3f0
Ploutarchos Michaelides <info@ploutarchosm.com>
2025-07-11 10:04:39 +0300

    add missing type

--- src/lib/controllers/translation.controller.ts
+++ src/lib/controllers/translation.controller.ts
@@ -14,7 +14,7 @@ import {
   PermissionsGuard,
   SECURITY_API_TOKEN_HEADER_KEY,
   TRANSLATOR,
-  QueriesForListRequest,
+  IListQuery,
 } from '@ploutos/common';
 import { TranslationService } from '../services/translation.service';
 import { CreateTranslationDTO } from '../dto/translation.dto';
@@ -60,7 +60,7 @@ export class TranslationController {
         },
       }),
     )
-    params: QueriesForListRequest,
+    params: IListQuery,
   ) {
     const [translations, count] = await this.translationService.list(
       params.take,
